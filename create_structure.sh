75183c7d24698edf2eee420b6dcf63c1abf3e7e9
TimLedger <tim_ledger@mail.ru>
2023-12-15 12:03:10 +0600

    задание 1

---
+++ create_structure.sh
@@ -0,0 +1,13 @@
+mkdir projects
+cd projects
+git init
+mkdir my_project
+cd my_project
+mkdir directory1
+mkdir directory1/subdirectory1
+mkdir directory2
+echo "Hello from JS" > directory2/hello.txt
+mkdir directory3
+mkdir directory3/subdirectory2
+
+chmod +x create_structure.sh 
