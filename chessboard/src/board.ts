eee0baafb0336f59949ee47555dcbb8445a738f3
TimLedger <139736485+TimLedger@users.noreply.github.com>
2023-12-15 12:43:21 +0600

    исправлена структура шахматной доски

--- chessboard/src/board.ts
+++ chessboard/src/board.ts
@@ -4,7 +4,7 @@ for (let i = 0; i < size; i++) {
   let row = '';
   for (let j = 0; j < size; j++) {
     if ((i + j) % 2 === 0) {
-      row += '██ ';
+      row += '██';
     } else {
       row += '  ';
     }
